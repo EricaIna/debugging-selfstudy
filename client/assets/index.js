f261bfd771e980601f64aff88b501f84ded7b989
“EricaIna” <erica.rirura.4you@gmail.com>
2023-11-02 10:36:19 +0000

    first commit

--- client/assets/index.js
+++ client/assets/index.js
@@ -1,40 +1,42 @@
 function displayQuote() {
-    const quote = {
-        content: "More than introversion or logic, though, coding selects for people who can handle endless frustration.",
-        author: "Clive Thompson"
-    };
+  const quote = {
+    content:
+      "More than introversion or logic, though, coding selects for people who can handle endless frustration.",
+    author: "Clive Thompson",
+  };
 
-    const textElement = document.querySelector("#text");
-    const authorElement = document.querySelector("#author");
+  const textElement = document.querySelector("#text");
+  const authorElement = document.querySelector("#author");
 
-    textElement.textContent = quote["content"];
-    authorElement.textContent = quote["author"];
+  textElement.textContent = quote["content"];
+  authorElement.textContent = quote["author"];
 }
 
 async function createNewQuote(e) {
+  e.preventDefault();
 
-    const data = {
-        text: e.target.name.value,
-        author: e.target.author.value
-    }
+  const data = {
+    text: e.target.name.value,
+    author: e.target.author.value,
+  };
 
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    }
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
 
-    const response = fetch("http://localhost:3000/quotes", options);
+  const response = await fetch("http://localhost:3000/quotes", options);
 
-    if (response.ok) {
-        alert("Quote added.");
-    }
+  if (response.ok) {
+    alert("Quote added.");
+  }
 }
 
 const form = document.querySelector("#create-form");
 form.addEventListener("submit", createNewQuote);
 
 const randomiseButton = document.querySelector("#btn-randomise");
-randomiseButton.addEventListener('click', displayquote);
+randomiseButton.addEventListener("click", displayQuote);
